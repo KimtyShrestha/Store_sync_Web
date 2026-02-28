2f433c2d938b9a29669c3bbff2b815c0b2bb7cd3
KimtyShrestha <kimtyshrestha@gmail.com>
2026-03-01 04:46:34 +0545

    Update global layout, styling, and axios configuration for API communication

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: "http://192.168.1.68:5050/api",
   headers: {
     "Content-Type": "application/json",
   },
